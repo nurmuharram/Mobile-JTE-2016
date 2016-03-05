fb9af9af9a50073c9875dc2329a6b3e1196aff19
Nur Muharram <nurmuh033@gmail.com>
2016-03-05 23:05:29 +0700

    Test Fragment Kiri Kanan

---
+++ FragmenTest/app/src/main/java/com/example/asus/fragmentest/Fragment_kiri.java
@@ -0,0 +1,22 @@
+package com.example.asus.fragmentest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Fragment_kiri extends Fragment {
+
+    public Fragment_kiri() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kiri, container, false);
+    }
+}
