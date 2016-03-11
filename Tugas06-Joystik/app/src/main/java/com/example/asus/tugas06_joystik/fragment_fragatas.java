e5ef4382ae99935990c87d23798c77744b73bfab
Nur Muharram <nurmuh033@gmail.com>
2016-03-11 21:25:41 +0700

    Tugas06-Joystik

---
+++ Tugas06-Joystik/app/src/main/java/com/example/asus/tugas06_joystik/fragment_fragatas.java
@@ -0,0 +1,22 @@
+package com.example.asus.tugas06_joystik;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class fragment_fragatas extends Fragment {
+
+    public fragment_fragatas() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fragatas, container, false);
+    }
+}
